c7e1902d05a2489e584c78e960539dd8baa4bf4c
aintShirou <atallado008@gmail.com>
2024-06-23 23:04:36 +0800

    validation checking

--- sale.php
+++ sale.php
@@ -191,6 +191,7 @@ require_once('classes/database.php');
 </div>
 
    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
   $(document).ready(function() {
     $('#addAccountForm').submit(function(e) {
